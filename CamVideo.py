40b7e711bd41397511869db0b550cdd5d388b7a1
Rakesh Kumar <111701024@smail.iitpkd.ac.in>
2019-12-08 00:01:44 +0530

    Video read from file and Camera

---
+++ CamVideo.py
@@ -0,0 +1,32 @@
+import cv2
+
+# camera
+# cap = cv2.VideoCapture(0)
+
+# file
+cap = cv2.VideoCapture('/root/Videos/Screencast_07-12-19_11:52:06 PM IST.webm')
+
+while cap.isOpened():
+    ret, frame = cap.read()
+    if ret == True:
+        cv2.imshow('Video', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    else:
+        break
+    
+
+cap.release()
+cv2.destroyAllWindows()
+
+'''
+while True:
+    ret, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    cv2.imshow('frame', gray)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+'''
