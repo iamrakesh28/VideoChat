616341e34e4ec0af56d70b58ceaa8b5424ef90a5
Rakesh Kumar <111701024@smail.iitpkd.ac.in>
2019-12-08 00:06:02 +0530

    isOpened() added

--- CamVideo.py
+++ CamVideo.py
@@ -6,11 +6,14 @@ import cv2
 # file
 cap = cv2.VideoCapture('/root/Videos/Screencast_07-12-19_11:52:06 PM IST.webm')
 
+if cap.isOpened() == False:
+    exit()
+    
 while cap.isOpened():
     ret, frame = cap.read()
     if ret == True:
         cv2.imshow('Video', frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+        if cv2.waitKey(100) & 0xFF == ord('q'):
             break
     else:
         break
