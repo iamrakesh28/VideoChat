3b1a1952f50f8757a18274f1d413be721fde0eac
Rakesh Kumar <111701024@smail.iitpkd.ac.in>
2020-04-05 23:09:47 +0530

    waitkey() parameter changed to 1
    1 is to display frame for 1 ms while 0 is for image

--- CamVideo.py
+++ CamVideo.py
@@ -1,10 +1,10 @@
 import cv2
 
 # camera
-# cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0)
 
 # file
-cap = cv2.VideoCapture('/root/Videos/Screencast_07-12-19_11:52:06 PM IST.webm')
+# cap = cv2.VideoCapture('/root/Videos/Screencast_07-12-19_11:52:06 PM IST.webm')
 
 if cap.isOpened() == False:
     exit()
@@ -13,7 +13,7 @@ while cap.isOpened():
     ret, frame = cap.read()
     if ret == True:
         cv2.imshow('Video', frame)
-        if cv2.waitKey(100) & 0xFF == ord('q'):
+        if cv2.waitKey(1) & 0xFF == ord('q'):
             break
     else:
         break
